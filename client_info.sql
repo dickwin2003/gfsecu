b7228656560d1d8b9d4d4357a301070e4ce8fa92
dickwin2003 <dickwin2003@gmail.com>
2017-12-05 18:31:25 +0800

    mark

---
+++ client_info.sql
@@ -0,0 +1,18 @@
+﻿-- Table: client_info
+
+-- DROP TABLE client_info;
+
+CREATE TABLE client_info
+(
+  id integer NOT NULL,
+  username text,
+  age integer,
+  password character(50),
+  name text,
+  CONSTRAINT client_info_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE client_info
+  OWNER TO postgres;
