5d26f9b1fe75371b242046124829865f6173be5a
dickwin2003 <dickwin2003@gmail.com>
2017-12-06 10:25:37 +0800

    modify the contorller

--- src/main/java/com/lvyuan/controller/UserController.java
+++ src/main/java/com/lvyuan/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.lvyuan.model.User;
 import com.lvyuan.service.UserService;
 
 @Controller
@@ -25,16 +24,23 @@ public class UserController {
     @RequestMapping("/getUserInfo")
     @ResponseBody
     
-    public List getUserInfo(@RequestParam(value="username", required=false) String username,@RequestParam(value="password", required=false) String password,@RequestParam(value="id", required=false) String id) {
+    public Map getUserInfo(@RequestParam(value="username", required=false) String username,@RequestParam(value="password", required=false) String password,@RequestParam(value="id", required=false) String id) {
     	Map paramMap = new HashMap();
     	paramMap.put("username",username);
     	paramMap.put("password",password);
     	paramMap.put("id",id);
-        List user = userService.getUserInfo(paramMap);
-//        if(user!=null){
-//            System.out.println("user.getName():"+user.getName());
-//            logger.info("user.getAge():"+user.getAge());
-//        }
-        return user;
+    	
+        HashMap result = new HashMap();
+		try {
+			List userlist = userService.getUserInfo(paramMap);
+			result.put("data",userlist ); 
+			result.put("len",userlist.size());  
+			result.put("ret", 0);  
+		} catch (Exception e) {
+			logger.error(e.getLocalizedMessage(), e);
+			result.put("data", "获取信息失败！");
+			result.put("ret", "1");
+		}
+        return result;
     }
 }
